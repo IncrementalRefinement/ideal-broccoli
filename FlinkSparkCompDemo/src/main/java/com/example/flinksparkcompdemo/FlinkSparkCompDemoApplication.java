87928ad2dc4e8d6385e76c95a7fcb3511beac20f
LTakamori <fluent_python@qq.com>
2022-04-08 18:52:33 +0800

    initialize FlinkSparkCompDemo

---
+++ FlinkSparkCompDemo/src/main/java/com/example/flinksparkcompdemo/FlinkSparkCompDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.flinksparkcompdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkSparkCompDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkSparkCompDemoApplication.class, args);
+    }
+
+}
