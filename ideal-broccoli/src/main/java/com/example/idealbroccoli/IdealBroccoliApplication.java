7a6f87257796f5c6b272fc7d7491bfd256cecf74
LTakamori <fluent_python@qq.com>
2022-04-12 20:31:07 +0800

    initialize ideal-broccoli

---
+++ ideal-broccoli/src/main/java/com/example/idealbroccoli/IdealBroccoliApplication.java
@@ -0,0 +1,13 @@
+package com.example.idealbroccoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealBroccoliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdealBroccoliApplication.class, args);
+    }
+
+}
---
+++ ideal-broccoli/src/test/java/com/example/idealbroccoli/IdealBroccoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idealbroccoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealBroccoliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
