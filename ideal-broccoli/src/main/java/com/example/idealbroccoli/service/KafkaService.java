d4526b372cbdbfd71cf987705a86abb876a376b8
LTakamori <fluent_python@qq.com>
2022-04-14 16:20:03 +0800

    finish KafkaService, without test

--- ideal-broccoli/src/main/java/com/example/idealbroccoli/service/KafkaService.java
+++ ideal-broccoli/src/main/java/com/example/idealbroccoli/service/KafkaService.java
@@ -1,5 +1,11 @@
 package com.example.idealbroccoli.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface KafkaService {
 
+    public void sendMessage(String message);
+
+    public void consumeMessage(String message);
 }
--- ideal-broccoli/src/main/java/com/example/idealbroccoli/service/impl/KafkaServiceImpl.java
+++ ideal-broccoli/src/main/java/com/example/idealbroccoli/service/impl/KafkaServiceImpl.java
@@ -1,6 +1,29 @@
 package com.example.idealbroccoli.service.impl;
 
 import com.example.idealbroccoli.service.KafkaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaServiceImpl implements KafkaService {
+
+    // TODO: read this from application.properties
+    private static final String GROUP_ID = "spark_event_group";
+    private static final String TOPIC_NAME = "spark_event_topic";
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    @Override
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC_NAME, message);
+    }
+
+    @Override
+    @KafkaListener(topics = TOPIC_NAME, groupId = GROUP_ID)
+    public void consumeMessage(String message) {
+        System.out.println(message);
+    }
 }
